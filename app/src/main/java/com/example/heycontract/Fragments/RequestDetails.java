f9a5f72b2c32047afe203500db779d6370ed7b33
Ethan Jansen <jansenethan06@gmail.com>
2019-11-12 17:45:53 +0200

    Update

--- app/src/main/java/com/example/heycontract/Fragments/RequestDetails.java
+++ app/src/main/java/com/example/heycontract/Fragments/RequestDetails.java
@@ -4,15 +4,28 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.heycontract.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class RequestDetails extends Fragment {
+	private CircleImageView detailsDisplayPicture;
+	private TextView detailsDisplayName;
+	private EditText edtTypeOfWork_Details;
+	private EditText edtDetails_Details;
+	private ImageButton btnDeclineQuote;
+	private ImageButton btnQuote;
 
 	public RequestDetails() {
 		// Required empty public constructor
@@ -25,6 +38,19 @@ public class RequestDetails extends Fragment {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_request_details, container, false);
 	}
-
 	
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		init();
+	}
+	
+	private void init() {
+		detailsDisplayPicture = getView().findViewById(R.id.detailsDisplayPicture);
+		detailsDisplayName = getView().findViewById(R.id.detailsDisplayName);
+		edtTypeOfWork_Details = getView().findViewById(R.id.edtTypeOfWork_Details);
+		edtDetails_Details = getView().findViewById(R.id.edtDetails_Details);
+		btnDeclineQuote = getView().findViewById(R.id.btnDeclineQuote);
+		btnQuote = getView().findViewById(R.id.btnQuote);
+	}
 }
